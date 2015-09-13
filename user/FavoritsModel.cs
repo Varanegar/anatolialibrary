9922829972f851f90fb4dbc95de11dec8371370e
ATP on thinkpad <aliasghar.toraby@gmail.com>
2015-09-13 15:56:22 +0430

    FavoiritsModel methods was added.

--- user/FavoritsModel.cs
+++ user/FavoritsModel.cs
@@ -1,4 +1,5 @@
-﻿using AnatoliaLibrary.products;
+﻿using AnatoliaLibrary.anatoliaclient;
+using AnatoliaLibrary.products;
 using AnatoliaLibrary.stores;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,57 @@ using System.Threading.Tasks;
 
 namespace AnatoliaLibrary.user
 {
-    class FavoritsModel<T> where T : ProductModel
+    class FavoritsModel : SyncDataModel
     {
+        Dictionary<ProductModel, int> _products;
+        public Dictionary<ProductModel, int> Products
+        {
+            get { return _products; }
+        }
+        public void AddItem(ProductModel product)
+        {
+            if (_products.ContainsKey(product))
+                _products[product]++;
+            else
+                _products.Add(product, 1);
+        }
+        public void AddItem(ProductModel product, int count)
+        {
+            if (_products.ContainsKey(product))
+                _products[product] += count;
+            else
+                _products.Add(product, count);
+        }
+        public void RemoveItem(ProductModel product)
+        {
+            try
+            {
+                _products.Remove(product);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        public void RemoveItem(ProductModel product, int count)
+        {
+            try
+            {
+                _products[product] -= count;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        public override void LocalSaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CloudSaveAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
