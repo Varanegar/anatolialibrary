a5340e889884b77f4a62b303198afdd8aba27368
hooman.ahmadi@gmail.com <hooman.ahmadi@gmail.com>
2015-09-17 12:10:01 +0430

    جا به جایی پروژه اندروید

---
+++ Source/V0.1/Android/AndroidFileIO.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AnatoliaLibrary.anatoliaclient;
+
+namespace AnatoliaAndroid
+{
+    class AndroidFileIO : IFileIO
+    {
+        public bool WriteAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
