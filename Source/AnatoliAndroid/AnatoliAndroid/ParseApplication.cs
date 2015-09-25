03a8a9b6574c9ea47fb8958a2de2f7a381c31962
ATP on thinkpad <aliasghar.toraby@gmail.com>
2015-09-25 14:54:59 +0330

    حذف فولدر های اضافی و تغییر نام library به packages

---
+++ Source/AnatoliAndroid/AnatoliAndroid/ParseApplication.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Parse;
+using Anatoli.App;
+
+namespace AnatoliAndroid
+{
+    [Application(Name = "anatoliandroid.ParseApplication")]
+    public class ParseApplication : Application
+    {
+        public ParseApplication(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            // Initialize the Parse client with your Application ID and .NET Key found on
+            // your Parse dashboard
+            ParseClient.Initialize(Configuration.parseAppId, Configuration.parseDotNetKey);
+            ParsePush.ParsePushNotificationReceived += ParsePush.DefaultParsePushNotificationReceivedHandler;
+
+        }
+    }
+}
