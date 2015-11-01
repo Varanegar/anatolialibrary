7b81e775240dafe3a80b1fcccf96d871a1730772
ATP on thinkpad <aliasghar.toraby@gmail.com>
2015-11-01 16:45:11 +0330

    Single line search text

--- Source/Device/AnatoliAndroid/AnatoliAndroid/Activities/MainActivity.cs
+++ Source/Device/AnatoliAndroid/AnatoliAndroid/Activities/MainActivity.cs
@@ -79,6 +79,7 @@ namespace AnatoliAndroid.Activities
             _searchEditText = _toolbar.FindViewById<AutoCompleteTextView>(Resource.Id.searchEditText);
             _autoCompleteOptions = new String[] { "نوشیدنی", "لبنیات", "پروتئینی", "خواربار", "روغن", "پنیر", "شیر", "ماست", "کره", "دوغ", "گوشت" };
             _autoCompleteAdapter = new ArrayAdapter(this, Resource.Layout.AutoCompleteDropDownLayout, _autoCompleteOptions);
+
             _searchEditText.Adapter = _autoCompleteAdapter;
             _searchEditText.TextChanged += async (s, e) =>
                 {
