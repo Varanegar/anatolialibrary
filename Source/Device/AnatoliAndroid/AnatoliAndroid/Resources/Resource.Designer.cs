bb9004dfd5250b7209456909999c699bdef9b969
ATP on thinkpad <aliasghar.toraby@gmail.com>
2015-11-03 10:37:40 +0330

    search back icon changed

--- Source/Device/AnatoliAndroid/AnatoliAndroid/Resources/Resource.Designer.cs
+++ Source/Device/AnatoliAndroid/AnatoliAndroid/Resources/Resource.Designer.cs
@@ -1383,76 +1383,79 @@ namespace AnatoliAndroid
 			public const int CheckoutGray = 2130837569;
 			
 			// aapt resource value: 0x7f020042
-			public const int Edit = 2130837570;
+			public const int Close = 2130837570;
 			
 			// aapt resource value: 0x7f020043
-			public const int Exit = 2130837571;
+			public const int Edit = 2130837571;
 			
 			// aapt resource value: 0x7f020044
-			public const int Favorits = 2130837572;
+			public const int Exit = 2130837572;
 			
 			// aapt resource value: 0x7f020045
-			public const int FavoritsGray = 2130837573;
+			public const int Favorits = 2130837573;
 			
 			// aapt resource value: 0x7f020046
-			public const int GroupIcon = 2130837574;
+			public const int FavoritsGray = 2130837574;
 			
 			// aapt resource value: 0x7f020047
-			public const int gwarrow = 2130837575;
+			public const int GroupIcon = 2130837575;
 			
 			// aapt resource value: 0x7f020048
-			public const int Help = 2130837576;
+			public const int gwarrow = 2130837576;
 			
 			// aapt resource value: 0x7f020049
-			public const int Icon = 2130837577;
+			public const int Help = 2130837577;
 			
 			// aapt resource value: 0x7f02004a
-			public const int Login = 2130837578;
+			public const int Icon = 2130837578;
 			
 			// aapt resource value: 0x7f02004b
-			public const int Map = 2130837579;
+			public const int Login = 2130837579;
 			
 			// aapt resource value: 0x7f02004c
-			public const int Menu = 2130837580;
+			public const int Map = 2130837580;
 			
 			// aapt resource value: 0x7f02004d
-			public const int MenuItem = 2130837581;
+			public const int Menu = 2130837581;
 			
 			// aapt resource value: 0x7f02004e
-			public const int Messages = 2130837582;
-			
-			// aapt resource value: 0x7f020059
-			public const int notification_template_icon_bg = 2130837593;
+			public const int MenuItem = 2130837582;
 			
 			// aapt resource value: 0x7f02004f
-			public const int Phone = 2130837583;
+			public const int Messages = 2130837583;
+			
+			// aapt resource value: 0x7f02005a
+			public const int notification_template_icon_bg = 2130837594;
 			
 			// aapt resource value: 0x7f020050
-			public const int Profile = 2130837584;
+			public const int Phone = 2130837584;
 			
 			// aapt resource value: 0x7f020051
-			public const int RemoveProductImage = 2130837585;
+			public const int Profile = 2130837585;
 			
 			// aapt resource value: 0x7f020052
-			public const int Search = 2130837586;
+			public const int RemoveProductImage = 2130837586;
 			
 			// aapt resource value: 0x7f020053
-			public const int SearchBack = 2130837587;
+			public const int Search = 2130837587;
 			
 			// aapt resource value: 0x7f020054
-			public const int Searchgreen = 2130837588;
+			public const int SearchBack = 2130837588;
 			
 			// aapt resource value: 0x7f020055
-			public const int ShoppingCard = 2130837589;
+			public const int Searchgreen = 2130837589;
 			
 			// aapt resource value: 0x7f020056
-			public const int ShoppingCardRed = 2130837590;
+			public const int ShoppingCard = 2130837590;
 			
 			// aapt resource value: 0x7f020057
-			public const int splash = 2130837591;
+			public const int ShoppingCardRed = 2130837591;
 			
 			// aapt resource value: 0x7f020058
-			public const int Store = 2130837592;
+			public const int splash = 2130837592;
+			
+			// aapt resource value: 0x7f020059
+			public const int Store = 2130837593;
 			
 			static Drawable()
 			{
