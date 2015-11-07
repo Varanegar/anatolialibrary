7972ae0cbd5a57b08b29aa6f5ce1e8853454c5af
ATP on thinkpad <aliasghar.toraby@gmail.com>
2015-11-07 13:50:38 +0330

    Order Model was added

---
+++ Source/Device/Framework V0.1/Anatoli/Anatoli/App/Model/Store/OrderItemModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anatoli.App.Model.Store
+{
+    public class OrderItemModel
+    {
+        public int order_id { get; set; }
+        public int product_id { get; set; }
+        public int product_count { get; set; }
+        public double product_price { get; set; }
+    }
+}
