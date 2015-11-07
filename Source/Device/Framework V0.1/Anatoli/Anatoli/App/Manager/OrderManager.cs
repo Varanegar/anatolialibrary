6ab2f6992f39802a4cf519954a255c340f24eaa3
ATP on thinkpad <aliasghar.toraby@gmail.com>
2015-11-07 13:47:38 +0330

    Method SaveOrder was added. (Transaction should be added)

--- Source/Device/Framework V0.1/Anatoli/Anatoli/App/Manager/OrderManager.cs
+++ Source/Device/Framework V0.1/Anatoli/Anatoli/App/Manager/OrderManager.cs
@@ -1,4 +1,5 @@
 ﻿using Anatoli.App.Model.Store;
+using Anatoli.Framework.AnatoliBase;
 using Anatoli.Framework.DataAdapter;
 using Anatoli.Framework.Manager;
 using System;
@@ -11,5 +12,52 @@ namespace Anatoli.App.Manager
 {
     public class OrderManager : BaseManager<BaseDataAdapter<OrderModel>, OrderModel>
     {
+        public static async Task<bool> SaveOrder()
+        {
+            try
+            {
+                var store = await StoreManager.GetDefault();
+                if (store != null)
+                {
+                    InsertCommand command = new InsertCommand("orders", new BasicParam("store_id", store.store_id.ToString()),
+                        new BasicParam("order_status", "0"),
+                        new BasicParam("order_date", DateTime.Now.ToLocalTime().ToString())
+                        );
+                    var result = await LocalUpdateAsync(command);
+                    if (result > 0)
+                    {
+                        var latestOrder = await GetLatestOrder();
+                        List<List<BasicParam>> parametres = new List<List<BasicParam>>();
+                        var items = await ShoppingCardManager.GetAllItemsAsync();
+                        foreach (var item in items)
+                        {
+                            var p = new List<BasicParam>();
+                            p.Add(new BasicParam("order_id", latestOrder.order_id));
+                            p.Add(new BasicParam("product_id", item.product_id.ToString()));
+                            p.Add(new BasicParam("product_count", item.count.ToString()));
+                            p.Add(new BasicParam("product_price", item.price.ToString()));
+                            parametres.Add(p);
+                        }
+                        InsertAllCommand command2 = new InsertAllCommand("order_items", parametres);
+                        var r = await LocalUpdateAsync(command2);
+                        if (r > 0)
+                        {
+                            await ShoppingCardManager.ClearAsync();
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static async Task<OrderModel> GetLatestOrder()
+        {
+            SelectQuery query = new SelectQuery("orders", new SortParam("order_id", SortTypes.DESC));
+            return await GetItemAsync(query);
+        }
     }
 }
