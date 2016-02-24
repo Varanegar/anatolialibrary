68c99d561f165b311af3ed53ebcc0060fc2e1c8a
ATP on thinkpad <aliasghar.toraby@gmail.com>
2016-02-24 16:58:55 +0330

    Product price bug fixed

--- Source/Device/Framework V0.1/Anatoli/Anatoli/App/Model/Store/ProductPriceUpdateModel.cs
+++ Source/Device/Framework V0.1/Anatoli/Anatoli/App/Model/Store/ProductPriceUpdateModel.cs
@@ -9,7 +9,7 @@ namespace Anatoli.App.Model.Store
 {
     public class ProductPriceUpdateModel : BaseDataModel
     {
-        public Guid StoreGuid { get; set; }
+        public string StoreGuid { get; set; }
         public string ProductGuid { get; set; }
         public decimal? Price { get; set; }
         public string StoreGuidString { get; set; }
