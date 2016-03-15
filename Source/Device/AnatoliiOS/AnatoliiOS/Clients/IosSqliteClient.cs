71fd8dc41513839bdfabdcd392fe7e590daf892f
ATP on thinkpad <aliasghar.toraby@gmail.com>
2016-03-15 12:26:40 +0330

    IOS project first commit. Anatoli clients implemented successfully.

---
+++ Source/Device/AnatoliiOS/AnatoliiOS/Clients/IosSqliteClient.cs
@@ -0,0 +1,21 @@
+﻿using Anatoli.Framework.AnatoliBase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace AnatoliiOS.Clients
+{
+    class IosSqliteClient : AnatoliSQLite
+    {
+        public override SQLite.SQLiteConnection GetConnection()
+        {
+            if (System.IO.File.Exists("paa.db"))
+            {
+                var conn = new SQLite.SQLiteConnection("paa.db");
+                return conn;
+            }
+            throw new FileNotFoundException("Database file not found");
+        }
+    }
+}
---
+++ Source/Device/AnatoliiOS/AnatoliiOS/Clients/IosWebClient.cs
@@ -0,0 +1,18 @@
+﻿using Anatoli.Framework.AnatoliBase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnatoliiOS.Clients
+{
+    class IosWebClient : AnatoliWebClient
+    {
+        public override bool IsOnline()
+        {
+            if (Reachability.InternetConnectionStatus() != NetworkStatus.NotReachable)
+                return false;
+            else
+                return true;
+        }
+    }
+}
