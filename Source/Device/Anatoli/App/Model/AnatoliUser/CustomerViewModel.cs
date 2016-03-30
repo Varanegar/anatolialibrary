608f9d920ec815f67d40c0e68eef94cdc7a8749e
ATP on mac <aliasghar.toraby@gmail.com>
2016-03-30 17:20:59 +0430

    Customer view model lower case id bug fixed.

--- Source/Device/Anatoli/App/Model/AnatoliUser/CustomerViewModel.cs
+++ Source/Device/Anatoli/App/Model/AnatoliUser/CustomerViewModel.cs
@@ -25,10 +25,14 @@ namespace Anatoli.App.Model
         public string MainStreet { get; set; }
         public string OtherStreet { get; set; }
         public string RegionInfoId { get; set; }
-        public string RegionLevel1Id { get; set; }
-        public string RegionLevel2Id { get; set; }
-        public string RegionLevel3Id { get; set; }
-        public string RegionLevel4Id { get; set; }
+		string _regionLevel1Id;
+		string _regionLevel2Id;
+		string _regionLevel3Id;
+		string _regionLevel4Id;
+		public string RegionLevel1Id { get { return (_regionLevel1Id == null) ? null : _regionLevel1Id.ToUpper (); } set{ _regionLevel1Id = value;} }
+		public string RegionLevel2Id { get { return (_regionLevel2Id == null) ? null : _regionLevel2Id.ToUpper (); } set { _regionLevel2Id = value;} }
+		public string RegionLevel3Id { get { return (_regionLevel3Id == null) ? null : _regionLevel3Id.ToUpper (); } set { _regionLevel3Id = value;} }
+		public string RegionLevel4Id { get{ return (_regionLevel4Id == null) ? null : _regionLevel4Id.ToUpper (); } set { _regionLevel4Id = value;} }
         public string DefauleStoreId { get; set; }
         //public List<BasketViewModel> Baskets { get; set; }
     }
