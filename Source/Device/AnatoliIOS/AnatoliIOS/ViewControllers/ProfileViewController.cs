5f54ef4b53f482dfe3c288ab0a4908f5169aad7c
ATP on mac <aliasghar.toraby@gmail.com>
2016-03-30 17:19:11 +0430

    Profile view added

--- Source/Device/AnatoliIOS/AnatoliIOS/ViewControllers/ProfileViewController.cs
+++ Source/Device/AnatoliIOS/AnatoliIOS/ViewControllers/ProfileViewController.cs
@@ -1,6 +1,10 @@
 using System;
 
 using UIKit;
+using CoreGraphics;
+using Anatoli.App.Model.Store;
+using System.Collections.Generic;
+using Anatoli.App.Manager;
 
 namespace AnatoliIOS.ViewControllers
 {
@@ -18,11 +22,116 @@ namespace AnatoliIOS.ViewControllers
             // Release any cached data, images, etc that aren't in use.
         }
 
-        public override void ViewDidLoad()
+        public async override void ViewDidLoad()
         {
             base.ViewDidLoad();
 
             // Perform any additional setup after loading the view, typically from a nib.
+			View.Bounds = UIScreen.MainScreen.Bounds;
+			nameTextField.Text = AnatoliApp.GetInstance ().Customer.FirstName;
+			lastNameTextField.Text = AnatoliApp.GetInstance ().Customer.LastName;
+			emailTextField.Text = AnatoliApp.GetInstance ().Customer.Email;
+			addressTextField.Text = AnatoliApp.GetInstance ().Customer.MainStreet;
+
+
+			var level1PickerModel = new CityRegionPickerViewModel (await CityRegionManager.GetFirstLevelAsync ());
+			var level2PickerModel = new CityRegionPickerViewModel (await CityRegionManager.GetGroupsAsync (AnatoliApp.GetInstance ().Customer.RegionLevel1Id));
+			var level3PickerModel = new CityRegionPickerViewModel (await CityRegionManager.GetGroupsAsync (AnatoliApp.GetInstance ().Customer.RegionLevel2Id));
+			var level4PickerModel = new CityRegionPickerViewModel (await CityRegionManager.GetGroupsAsync (AnatoliApp.GetInstance ().Customer.RegionLevel3Id));
+
+			level1Picker.Model = level1PickerModel;
+			level2Picker.Model = level2PickerModel;
+			level3Picker.Model = level3PickerModel;
+			level4Picker.Model = level4PickerModel;
+
+			SelectByGroupId (level1Picker, AnatoliApp.GetInstance ().Customer.RegionLevel1Id);
+			SelectByGroupId (level2Picker, AnatoliApp.GetInstance ().Customer.RegionLevel2Id);
+			SelectByGroupId (level3Picker, AnatoliApp.GetInstance ().Customer.RegionLevel3Id);
+			SelectByGroupId (level4Picker, AnatoliApp.GetInstance ().Customer.RegionLevel4Id);
+
+			level1PickerModel.ItemSelected += async (item) => {
+				if(item != null){
+					(level2Picker.Model as CityRegionPickerViewModel).SetItems(await CityRegionManager.GetGroupsAsync (item.group_id));
+					level2Picker.ReloadComponent(0);
+				}
+				level2Picker.Select(0,0,true);
+				level3Picker.Select(0,0,true);
+				level4Picker.Select(0,0,true);
+			};
+			level2PickerModel.ItemSelected += async (item) => {
+				if(item != null){
+					(level3Picker.Model as CityRegionPickerViewModel).SetItems(await CityRegionManager.GetGroupsAsync (item.group_id));
+					level3Picker.ReloadComponent(0);
+				}
+				level3Picker.Select(0,0,true);
+				level4Picker.Select(0,0,true);
+			};
+			level3PickerModel.ItemSelected += async (item) => {
+				if(item != null){
+					(level4Picker.Model as CityRegionPickerViewModel).SetItems(await CityRegionManager.GetGroupsAsync (item.group_id));
+					level4Picker.ReloadComponent(0);
+				}
+				level4Picker.Select(0,0,true);
+			};
+
+			saveButton.TouchUpInside += (object sender, EventArgs e) => {
+
+
+			};
+
         }
+		public void SelectByGroupId(UIPickerView picker,  string groupId){
+			var regionPicker = (picker.Model as CityRegionPickerViewModel);
+			if (regionPicker.ItemsDictionary.ContainsKey(groupId)) {
+				picker.Select (regionPicker.ItemsDictionary [groupId], 0, true);
+			}
+		}
     }
+
+	class CityRegionPickerViewModel : UIPickerViewModel{
+		List<CityRegionModel> _items;
+		public Dictionary <string,int> ItemsDictionary;
+		public void SetItems(List<CityRegionModel> items){
+			_items = items;
+			_items.Insert (0,null);
+			ItemsDictionary = new Dictionary<string, int> ();
+			for (int i = 0; i < _items.Count; i++) {
+				if (_items[i] != null) {
+					ItemsDictionary.Add (_items[i].group_id, i);
+				}
+			}
+		}
+		public CityRegionPickerViewModel(List<CityRegionModel> items){
+			SetItems(items);
+		}
+
+		public override nint GetComponentCount (UIPickerView pickerView)
+		{
+			return 1;
+		}
+		public override nint GetRowsInComponent (UIPickerView pickerView, nint component)
+		{
+			return _items.Count;
+		}
+		public override string GetTitle (UIPickerView pickerView, nint row, nint component)
+		{
+			if (_items [(int)row] != null)
+				return _items [(int)row].group_name;
+			else
+				return "";
+		}
+
+		public override void Selected (UIPickerView pickerView, nint row, nint component)
+		{
+				OnSelected (_items[(int)row]);
+		}
+		void OnSelected(CityRegionModel item){
+			if (ItemSelected != null) {
+				ItemSelected.Invoke (item);
+			}
+		}
+		public event ItemSelectedEventHandler ItemSelected;
+		public delegate void ItemSelectedEventHandler(CityRegionModel item);
+	}
+
 }
