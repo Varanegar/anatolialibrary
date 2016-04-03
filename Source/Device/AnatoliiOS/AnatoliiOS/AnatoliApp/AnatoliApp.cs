cc9b776d21a5c59224b2143e3a0ff5ae36e76226
ATP on mac <aliasghar.toraby@gmail.com>
2016-04-03 13:19:51 +0430

    Change color of the navigation bar and font of items

--- Source/Device/AnatoliiOS/AnatoliiOS/AnatoliApp/AnatoliApp.cs
+++ Source/Device/AnatoliiOS/AnatoliiOS/AnatoliApp/AnatoliApp.cs
@@ -183,4 +183,15 @@ namespace AnatoliIOS
 			ReplaceViewController (new FirstPageViewController ());
 		}
 	}
+
+	public static class Extensions{
+			public static UIColor FromHex(this UIColor color,int hexValue)
+			{
+				return UIColor.FromRGB(
+					(((float)((hexValue & 0xFF0000) >> 16))/255.0f),
+					(((float)((hexValue & 0xFF00) >> 8))/255.0f),
+					(((float)(hexValue & 0xFF))/255.0f)
+				);
+			}
+	}
 }
