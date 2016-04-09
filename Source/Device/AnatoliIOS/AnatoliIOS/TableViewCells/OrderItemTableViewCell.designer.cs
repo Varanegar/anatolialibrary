f12f2c5cc91006293527175706e0bbaa0cba2678
ATP on mac <aliasghar.toraby@gmail.com>
2016-04-09 15:32:56 +0430

    order items table added

---
+++ Source/Device/AnatoliIOS/AnatoliIOS/TableViewCells/OrderItemTableViewCell.designer.cs
@@ -0,0 +1,18 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+
+namespace AnatoliIOS
+{
+	[Register ("OrderItemTableViewCell")]
+	partial class OrderItemTableViewCell
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
