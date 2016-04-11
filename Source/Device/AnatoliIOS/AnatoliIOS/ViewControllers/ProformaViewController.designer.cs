50ece95d2aaee4782a0f0f973ba1158b998fd056
ATP on mac <aliasghar.toraby@gmail.com>
2016-04-11 11:34:29 +0430

    Pro forma view completed

--- Source/Device/AnatoliIOS/AnatoliIOS/ViewControllers/ProformaViewController.designer.cs
+++ Source/Device/AnatoliIOS/AnatoliIOS/ViewControllers/ProformaViewController.designer.cs
@@ -16,10 +16,34 @@ namespace AnatoliIOS.ViewControllers
 	{
 		[Outlet]
 		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton cancelButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIView footer { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIView header { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
 		UITableView table { get; set; }
 
 		void ReleaseDesignerOutlets ()
 		{
+			if (cancelButton != null) {
+				cancelButton.Dispose ();
+				cancelButton = null;
+			}
+			if (footer != null) {
+				footer.Dispose ();
+				footer = null;
+			}
+			if (header != null) {
+				header.Dispose ();
+				header = null;
+			}
 			if (table != null) {
 				table.Dispose ();
 				table = null;
