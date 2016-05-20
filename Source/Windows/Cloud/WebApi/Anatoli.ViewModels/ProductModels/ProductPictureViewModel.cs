f9724ceda674b12aba7c5eec35c3e1d5162b3892
MohammadReza Gorouhian <gorouhian@gmail.com>
2016-05-20 19:39:52 +0430

    Adding ProtobufSampleController.

--- Source/Windows/Cloud/WebApi/Anatoli.ViewModels/ProductModels/ProductPictureViewModel.cs
+++ Source/Windows/Cloud/WebApi/Anatoli.ViewModels/ProductModels/ProductPictureViewModel.cs
@@ -1,12 +1,13 @@
-using System;
-using System.Linq;
-using System.Collections.Generic;
+using ProtoBuf;
 
 namespace Anatoli.ViewModels.ProductModels
 {
+    [ProtoContract]
     public class ProductPictureViewModel : BaseViewModel
     {
+        [ProtoMember(1)]
         public string ProductPictureName { get; set; }
+        [ProtoMember(2)]
         public bool IsDefault { get; set; }
     }
 }
