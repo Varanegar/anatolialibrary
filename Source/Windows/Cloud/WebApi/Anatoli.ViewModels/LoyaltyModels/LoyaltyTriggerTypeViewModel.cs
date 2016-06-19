4508985ba492decbfe897eb9066e29d9285c2bc4
hooman.ahmadi@gmail.com <hooman.ahmadi@gmail.com>
2016-06-19 16:29:43 +0430

    Move view model to shared library

--- Source/Windows/Cloud/WebApi/Anatoli.ViewModels/LoyaltyModels/LoyaltyTriggerTypeViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Anatoli.ViewModels.LoyaltyModels
-{
-    //Historical, PerTransaction
-    public class LoyaltyTriggerTypeViewModel : BaseViewModel
-    {
-        public string LoyaltyTriggerTypeName { get; set; }
-       // public List<LoyaltyRule> LoyaltyRules { get; set; }
-    }
-}
